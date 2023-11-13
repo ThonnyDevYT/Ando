e803036f44e29e79e010e9f26f550f6fc79e2b46
ThonnyDev <97204749+ThonnyDevYT@users.noreply.github.com>
2023-11-13 18:00:35 -0500

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,7 +1,7 @@
 package;
 
 #if android
-import android.AndroidTools;
+//import android.AndroidTools;
 import android.Permissions;
 #end
 import lime.app.Application;
@@ -17,7 +17,7 @@ class SUtil
     #if android
     private static var aDir:String = null;
     private static var sPath:String = AndroidTools.getExternalStorageDirectory();  
-    private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions();  
+    //private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions();  
     #end
 
     static public function getPath():String
@@ -40,19 +40,6 @@ class SUtil
     static public function doTheCheck()
     {
         #if android
-        if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
-            if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
-            }  
-        }
-
-        if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
-            if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                SUtil.applicationAlert("Permissions", "If you accepted the permisions for storage, good, you can continue, if you not the game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App");
-            } else {
-                SUtil.applicationAlert("Permissions", "The Game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App");
-            }
-        }
 
         if (!FileSystem.exists(sPath + "/" + "." + Application.current.meta.get("file"))){
             FileSystem.createDirectory(sPath + "/" + "." + Application.current.meta.get("file"));
